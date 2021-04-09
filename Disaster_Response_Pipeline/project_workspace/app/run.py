be33ccc8bedf3eabb09613308b050e2e9d30df7b
Chrysanthi Polyzoni <49782010+chpolyzo@users.noreply.github.com>
2021-04-09 22:36:27 +0200

    update classifier.pkl in the run.py file

--- Disaster_Response_Pipeline/project_workspace/app/run.py
+++ Disaster_Response_Pipeline/project_workspace/app/run.py
@@ -30,7 +30,7 @@ engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('response_table', engine)
 
 # load model
-model = joblib.load("../models/classifier")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
